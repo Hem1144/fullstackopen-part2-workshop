731d8426591fe5d0bcdd5375fad0416a6f55b772
Hemlal Dulal <dulalhemlal986@gmail.com>
2023-07-25 15:01:52 +0545

    feat: completed workshop of 2-b

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import Refactor from "./components/Refactor";
+import { useState } from "react";
+import Note from "./components/Note";
 
-function App({ notes }) {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = notes.filter((note) => (showAll ? true : note.important));
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setNotes(
+      notes.concat({
+        tech: newNote,
+        id: notes.length + 1,
+        important: Math.random() > 0.5,
+      })
+    );
+    setNewNote("");
+    console.log("form has been submitted");
+  };
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const handleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <>
-      <h1>My Note</h1>
-      <Refactor notes={notes} />
+      <h1>Notes</h1>
+      <button onClick={handleShowAll}>
+        show {showAll ? "important" : "all"}
+      </button>
+      <ul>
+        {notesToShow.map((value) => {
+          return <Note key={value.id} note={value} />;
+        })}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input value={newNote} onChange={handleChange} />
+        <button>Submit</button>
+      </form>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Note.js
@@ -0,0 +1,5 @@
+const Note = ({ note }) => {
+  return <li>{note.tech}</li>;
+};
+
+export default Note;
